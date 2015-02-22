130289f77a2ceeeffa49703d1aa5b111325979b7
unknown <melvin.jc@gmail.com>
2015-02-22 23:19:26 +0000

    Mistake in previous commit, left out modified files. Previous commit log still applies to here:
    Commiting first-pass results.
    
    cacheMatrix.R completed
    - makeCacheMatrix functioning, incomplete comments
    - cacheSolve completed, added mod for turning off debug message
        - This speeds up the cached value loading and allows benchmarking
        - Printing to stdout() seems to take a lot of time on my
          machine
    
    test1.R
    - Functionality testing of both functions
    - Benchmark printout of both first calculation and second that loads
      cached inverse
    
    test2.R
    - Quick test of <<- operator, how despite using x_inv in makeCacheMatrix,
      does not create conflict in scoping
        - Docs indicate that <<- will cause search upwards in environments
          and assign to variable if found

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,74 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+#  makeCacheMatrix
+#
+#  Create a cacheMatrix object, which stores a matrix, and also can be used to calculate
+#  and store the inverse of that matrix
+#
+#  Returns the cacheMatrix object created.
+#
+#  Inputs:
+#  x   -  A matrix to be used as the initial matrix of this object. Default is an empty matrix.
+#
+#  Member functions:
+#  set(y)      -  Set the matrix to y. Clears the previous cached inverse value.
+#  get         -  Accessor function that returns the matrix
+#  setinv(inv) -  Set the inverse of the matrix to z.
+#                 Note: there are no checks if z is the correct inverse of the matrix
+#  getinv      -  Accessor function that returns the cached inverse of the matrix if already calculate,
+#                 or calculates the inverse of the matrix first before returning if not yet calculated,
+#                    after which it is cached.
 makeCacheMatrix <- function(x = matrix()) {
-
+   # Initialise an empty matrix to hold the inverse of this matrix 
+   x_inv <- NULL
+   
+   # Now define the member functions of the cacheMatrix object
+   
+   # 
+   set <- function(y) {
+      # Use <<- so 
+      x     <<- y
+      # Clear
+      x_inv <<- NULL
+   }
+   # Return the matrix itself
+   get <- function() x
+   setinv <- function(inv) x_inv <<- inv
+   # Return the stored matrix inverse, even if it's still null
+   getinv <- function() x_inv
+   
+   # Return the cacheMatrix object, as a list containing references to the member methods
+   list(set = set, get = get,
+        setinv = setinv,
+        getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+# cacheSolve
+#
+# Solves the inverse of a matrix in a cacheMatrix object.
+# Will load the cached result if available, instead of calculating again.
+#
+# Returns the inverse matrix
+#
+# x         -  cacheMatrix object to pass in and solve
+# msg_ena   -  If TRUE, will print debug messages (default=TRUE)
+cacheSolve <- function(x, msg_ena=TRUE, ...) {
+   # Get the inverse stored in the cacheMatrix object.
+   x_inv <- x$getinv()
+   # If the inverse is not null, it exists and has already bee calculated and cached.
+   # Simply return the cached value
+   if(!is.null(x_inv)) {
+      # Print debug message if wanted
+      if(msg_ena==TRUE) {
+         message("Getting cached inverse for the matrix")
+      }
+      return(x_inv)
+   }
+   # If the inverse does not yet exist (x_inv==null), calculate it here
+   # Get the matrix
+   data <- x$get()
+   # Calculate the matrix inverse and store in x_inv
+   x_inv <- solve(data, ...)
+   # Cache the matrix inverse in the cacheMatrix object
+   x$setinv(x_inv)
+   # Return the calculated matrix inverse
+   x_inv
 }
--- test1.R
+++ test1.R
@@ -1,3 +1,18 @@
+# Test simple functionality of cacheMatrix.R functions
+# and benchmark caching performance
+
+
+# Get simple benchmarking if possible
+if("microbenchmark" %in% rownames(installed.packages())) {
+   library(microbenchmark)
+   bm = microbenchmark
+} else {
+   # If not available, just run whatever it is that needs running
+   bm = function(x,...) x
+   warning("microbenchmark package not installed, so benchmark results not valid. 
+            Run install.packages(\"microbenchmark\").")
+}
+
 source("cachematrix.R")
 # Create a matrix
 a = matrix(c(4,3,3,2),nrow=2,ncol=2)
@@ -33,3 +48,14 @@ write(b_inv, stdout())
 
 # Check that solution is correct
 all.equal(a_inv, b_inv)
+
+message("=======================================================")
+message("Do some benchmarking")
+c = makeCacheMatrix(a)
+bm_text = bm(cacheSolve(c), times = 1)
+message("First time calculate")
+print(bm_text)
+bm_text
+message("Using cached value")
+bm_text = bm(cacheSolve(c, msg_ena=FALSE), times = 5)
+print(bm_text)
